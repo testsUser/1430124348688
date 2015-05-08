b1de94c07b1dfb9b814e93598116687fa199016d
Tomek Stysial <tomekstysial@o2.pl>
2015-05-08 15:19:02 +0200

    Zrobione

--- app/app.js
+++ app/app.js
@@ -1,19 +1,32 @@
 'use strict';
 var app = angular.module('app', []);
 
-app.controller('AppCtrl', function () {
+app.controller('AppCtrl', function ($http) {
     var ctrl = this;
     ctrl.favouriteSong = {artist: 'Ed Sheeran', title: 'I see fire'};
 
     // add get query
+    $http.get("http://localhost:3000/songs")
+        .success(function(songsList) {
+            ctrl.songs = songsList;
+        });
     // add get query
 
     ctrl.addFavouriteSong = function (favouriteSong) {
         // complete function
+        $http.post("http://localhost:3000/favourite_songs", favouriteSong)
+            .success(function(favouriteSongsList) {
+                ctrl.favouriteSongs = favouriteSongsList;
+            })
+
     };
 
     ctrl.removeFavouriteSongs = function () {
         //complete function
+        $http.post("http://localhost:3000/favourite_songs")
+            .success(function() {
+                ctrl.favouriteSongs = [];
+            })
 
     };
 });
